4831976d5f03f6b51b4bd21c3b5bdb0192bc393f
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-03-10 16:57:10 +0000

    routes and test routes for user registration

--- app.py
+++ app.py
@@ -15,8 +15,21 @@ app = Flask(__name__)
 def get_index():
     return render_template('index.html')
 
+@app.route('/register', methods=['GET'])
+def get_register():
+    connection = get_flask_database_connection(app)
+    return render_template('register_user.html')
+
+@app.route('/register', methods=['POST'])
+def send_new_registration():
+    connection = get_flask_database_connection(app)
+    repository = UserRepository(connection)
+    new_user = User(request.form)
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
+
