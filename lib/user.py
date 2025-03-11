57161b1a85b76e56610ade2386762a7fe35db1f1
Jack Misner <jack.misner@gmail.com>
2025-03-11 16:24:14 +0000

    Changed passwords in database to be encrypted

--- lib/user.py
+++ lib/user.py
@@ -1,13 +1,29 @@
+import hashlib
 class User:
-    def __init__(self, id, username, password):
+    def __init__(self, id: int, username: str, unencrypted_password: str):
         self.id = id
         self.username = username
-        self.password = password
+        self.encrypted_password = hashlib.md5(unencrypted_password.encode()).hexdigest()
 
     def __repr__(self):
         return (
-            f"User(ID: {self.id}, Username: {self.username}, Password: {self.password})"
+            f"User(ID: {self.id}, Username: {self.username}, Password: {self.encrypted_password})"
         )
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
+    @classmethod
+    def from_database(cls, id: int, username: str, encrypted_password: str):
+        """
+        Alternative constructor for creating a User from database data
+        Allows direct creation with a pre-existing hash
+        """    
+        # Create a new instance
+        instance = cls.__new__(cls)
+        
+        # Set attributes directly
+        instance.id = id
+        instance.username = username
+        instance.encrypted_password = encrypted_password        
+        return instance
--- lib/user_repository.py
+++ lib/user_repository.py
@@ -1,18 +1,18 @@
 from lib.user import User
-
+import hashlib
 class UserRepository:
     def __init__(self, connection):
         self._connection = connection
 
     def all(self):
         rows = self._connection.execute("SELECT * FROM users")
-        return [User(row["id"], row["username"], row["password"]) for row in rows]
+        return [User(row["id"], row["username"], row["encrypted_password"]) for row in rows]
 
     def create(self, user):
         if self._isUserUnique(user.username):  
             self._connection.execute(
-                "INSERT INTO users (username, password) VALUES (%s, %s)",
-                [user.username, user.password],
+                "INSERT INTO users (username, encrypted_password) VALUES (%s, %s)",
+                [user.username, user.encrypted_password],
             )
         return None
     
@@ -25,15 +25,22 @@ class UserRepository:
     def find_by_id(self, id: int):
         rows = self._connection.execute("SELECT * from users WHERE id = %s", [id])
         row = rows[0]
-        return User(row["id"], row["username"], row["password"])
+        return User.from_database(row["id"], row["username"], row["encrypted_password"])
     
     def find_by_username(self, username: str):
         rows = self._connection.execute("SELECT * from users WHERE username = %s", [username])
         row = rows[0]
-        return User(row["id"], row["username"], row["password"])
+        return User.from_database(row["id"], row["username"], row["encrypted_password"])
     
     def list_all_usernames(self):
         usernames = []
         for user in self.all():
             usernames.append(user.username)
         return usernames
+    
+    def check_password(self, username: str, unencrypted_password: str) -> bool:
+            password_hash = hashlib.md5(unencrypted_password.encode()).hexdigest()
+            test_user = self.find_by_username(username)
+            if not isinstance(test_user, User):
+                return False
+            return test_user.encrypted_password == password_hash
