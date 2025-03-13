ea5c8c6efc9df6f10e6787a8e12e23341e23fd42
Andrew <therealmon8@gmail.com>
2025-03-13 11:02:34 +0000

    spelling error

--- app.py
+++ app.py
@@ -191,9 +191,9 @@ def display_about_page():
         {'name': 'Andrew Pang', 'image': "https://ca.slack-edge.com/T03ALA7H4-U089649MMQC-c22713126f2f-512", "ghprofile": 'https://github.com/pangacm'},
         {'name': 'Will Egerton', 'image': "https://ca.slack-edge.com/T03ALA7H4-U089SD1E83A-9fef626c96b4-512", "ghprofile": 'https://github.com/WEgerton'},
         {'name': 'Jack Misner', 'image': "https://ca.slack-edge.com/T03ALA7H4-U089CLJQMKK-9b3e6a0e85de-512", "ghprofile": 'https://github.com/jackmisner'},
-        {'name': 'Joseph Ducrocq"', 'image': "https://ca.slack-edge.com/T03ALA7H4-U088KDUVD0F-c40d5d623bb1-512", "ghprofile": 'https://github.com/JosephDucrocq'},
+        {'name': 'Joseph Ducrocq', 'image': "https://ca.slack-edge.com/T03ALA7H4-U088KDUVD0F-c40d5d623bb1-512", "ghprofile": 'https://github.com/JosephDucrocq'},
         {'name': 'John Rothera', 'image': "https://ca.slack-edge.com/T03ALA7H4-U0893FT4Q7M-cd53f939148c-512", "ghprofile": 'https://github.com/JohnRothera'},
-        {'name': 'Luis Moseley-Robinson"', 'image': "https://ca.slack-edge.com/T03ALA7H4-U089649HLAG-f31e2ebbfeab-512", "ghprofile": 'https://github.com/fastongithub'}
+        {'name': 'Luis Moseley-Robinson', 'image': "https://ca.slack-edge.com/T03ALA7H4-U089649HLAG-f31e2ebbfeab-512", "ghprofile": 'https://github.com/fastongithub'}
     ]
 
     if "username" in session and session["username"] != None:
